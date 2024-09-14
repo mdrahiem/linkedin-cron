7ec6f385dcead2a8796932cc291e3a15382c3db0
rahimuddin.mohammad <rahimuddin.mohammad@postcodeloterij.nl>
2024-09-14 08:35:28 +0200

    implement google sheets data extraction
    Installed google apis, integrated credentials and logged the sheet data

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const cron = require("node-cron");
+const { getSheetData } = require("./getPosts");
 
 cron.schedule("*/10 * * * * *", () => {
+  getSheetData();
   console.log("Running a task every hour");
   // Add your scheduled task logic here
 });
